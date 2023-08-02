2b4c098c670acc340288b1692842f11133ae3cb9
bryan <bryan.oliveira12345@gmail.com>
2023-08-02 11:29:13 -0300

    forms and register initial

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,8 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('', include('recipes.urls')),  # /
+    path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
     path('admin/', admin.site.urls),
 ]
 
