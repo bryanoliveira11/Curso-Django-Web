4aea489bad24ed3daccfb25bf43d0d57b8e72547
bryan <bryan.oliveira12345@gmail.com>
2023-08-23 13:42:36 -0300

    json response

--- recipes/urls.py
+++ recipes/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
          views.RecipeListViewCategory.as_view(), name='category'),
     path('recipes/<int:pk>/',
          views.RecipeDetailView.as_view(), name='recipe'),
+    path('recipes/api/v1/', views.RecipeListViewHomeApi.as_view(),
+         name='recipes_api_v1'),
+    path('recipes/api/v1/<int:pk>/', views.RecipeDetailViewApi.as_view(),
+         name='recipes_api_v1_detail'),
 ]
