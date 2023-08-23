577ef0358ae53522f16fa6ab1c5d254e594fca88
bryan <bryan.oliveira12345@gmail.com>
2023-08-23 13:54:21 -0300

    django debug toolbar

--- project/settings.py
+++ project/settings.py
@@ -46,10 +46,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,3 +151,8 @@ MESSAGE_TAGS = {
     constants.INFO: 'message-info',
     constants.WARNING: 'message-warning',
 }
+
+# debug toolbars internal ips
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
