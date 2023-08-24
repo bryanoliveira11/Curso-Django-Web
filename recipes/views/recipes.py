edfd24d94889c781e0dd5404837e1bcbd02c7b2d
bryan <bryan.oliveira12345@gmail.com>
2023-08-24 14:18:47 -0300

    more query theory tests

--- recipes/views/recipes.py
+++ recipes/views/recipes.py
@@ -1,8 +1,9 @@
 from os import environ
 from typing import Any, Dict
 
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+# from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F, Q
+from django.db.models.aggregates import Count
 from django.forms.models import model_to_dict
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
@@ -22,13 +23,40 @@ def theory(request, *args, **kwargs):
     # recipes = recipes.filter(title__icontains='Created').order_by('-id').last()
     # recipes = Recipe.objects.get(id=1)
 
-    try:
-        recipes = Recipe.objects.get(id=1000)
-    except ObjectDoesNotExist:
-        recipes = None
+    # try:
+    #     recipes = Recipe.objects.get(id=1000)
+    # except ObjectDoesNotExist:
+    #     recipes = None
+
+    # recipes = Recipe.objects.filter(
+    #     Q(
+    #         Q(title__icontains='a', id__gt=2) |
+    #         Q(id__gt=1000)
+    #     ),
+    # )[:10]
+
+    # F An object capable of resolving references to existing query objects.
+    # recipes = Recipe.objects.filter(
+    #     id=F('author__id')).order_by('-id', 'title')
+
+    # .values returns a dict
+    # recipes = Recipe.objects.values('id', 'title', 'author__username')[:10]
+
+    # .only returns all fields that you select
+    # recipes = Recipe.objects.only('id', 'title', 'is_published')
+
+    # .defer returns all fields except the ones you select
+    # recipes = Recipe.objects.defer('is_published')
+
+    # Count
+    recipes = Recipe.objects.values('id', 'title').filter(
+        title__icontains='rib'
+    )
+    number_of_recipes = recipes.aggregate(number=Count('id'))
 
     context = {
-        'recipes': recipes
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number']
     }
 
     return render(
